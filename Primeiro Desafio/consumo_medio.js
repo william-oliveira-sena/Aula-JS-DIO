672a2d70b733821d66676e8add78793078bcf8cf
william-oliveira-sena <92284934+william-oliveira-sena@users.noreply.github.com>
2023-04-21 00:24:02 -0300

    Atualiz p/ função para o calc e html

--- Primeiro Desafio/consumo_medio.js
+++ Primeiro Desafio/consumo_medio.js
@@ -1,8 +1,32 @@
-const precoCombustivel = 5.79;
-const KmPorLitro = 12;
-const distanciaEmKm = 1580;
+function calcular(){
+    
+        totalcad = 0;
+        consumos = [];
 
-const litrosConsumidos = distanciaEmKm / KmPorLitro;
-const valorGasto = litrosConsumidos * precoCombustivel;
+    const precoCombustivel = parseFloat(document.getElementById("precocombustivel").value);
+    const KmPorLitro = parseFloat(document.getElementById("kmporlitro").value);
+    const distanciaEmKm = parseFloat(document.getElementById("distanciaemkm").value);
 
-console.log(valorGasto.toFixed(2));
+        //let distanciaEmKm1 = parseFloat(distanciaEmKm);
+       // let KmPorLitro1 = parseFloat(KmPorLitro);
+       // let precoCombustivel1 = parseFloat(precoCombustivel);
+
+        const litrosConsumidos = distanciaEmKm / KmPorLitro;
+        const valorGasto = litrosConsumidos * precoCombustivel;
+
+      //console.log (valorGasto);
+
+      let consumo = {preco:precoCombustivel, kmlitro:KmPorLitro, distanciaEmKm:distanciaEmKm, valortotal:valorGasto};
+      consumos[totalcad] = consumo;
+
+      let elemento = document.getElementById("saida");
+
+      elemento.innerHTML += "preco combustivel" + consumos[totalcad].preco +"<br>" + 
+                            "km por litro" + consumos[totalcad].kmlitro + "<br>" +
+                            "distancia total" + consumos[totalcad].distanciaEmKm +"<br>" +
+                            "valor total gasto" + consumos[totalcad].valortotal;
+
+      totalcad ++;
+    }
+
+//console.log(valorGasto.toFixed(2));
