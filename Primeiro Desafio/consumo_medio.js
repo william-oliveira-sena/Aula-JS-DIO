278f141b5b4d4a6d00b4a40b0fc62076876cff57
william-oliveira-sena <92284934+william-oliveira-sena@users.noreply.github.com>
2023-04-21 23:09:31 -0300

    Aula 2 - adicionando tipo de combustível

--- Primeiro Desafio/consumo_medio.js
+++ Primeiro Desafio/consumo_medio.js
@@ -3,21 +3,29 @@ function calcular(){
         totalcad = 0;
         consumos = [];
 
-    var precoCombustivel = document.getElementById("precocombustivel").value;
+    var precoGasolina = document.getElementById("precogasolina").value;
+    var precoEtanol = document.getElementById("precoetanol").value;
+    var combustivel = document.getElementById("combustivelutilizado").value;
     var KmPorLitro = document.getElementById("kmporlitro").value;
     var distanciaEmKm = document.getElementById("distanciaemkm").value;
 
-        
+        if(combustivel == "gasolina" || combustivel == "Gasolina"){
 
-        var litrosConsumidos = distanciaEmKm / KmPorLitro;
-        var valorGasto = litrosConsumidos * precoCombustivel;
+              var litrosConsumidos = distanciaEmKm / KmPorLitro;
+              var valorGasto = litrosConsumidos * precoGasolina;
+      }else if(combustivel == "etanol" || combustivel == "Etanol"){
 
+              var litrosConsumidos = distanciaEmKm / KmPorLitro;
+              var valorGasto = litrosConsumidos * precoEtanol;
+      }
       
 
-      var consumo = {preco:precoCombustivel, kmlitro:KmPorLitro, distanciaEmKm:distanciaEmKm, valortotal:valorGasto};
+      var consumo = {precogasolina:precoGasolina, precoetanol:precoEtanol, combust:combustivel, kmlitro:KmPorLitro, distanciaEmKm:distanciaEmKm, valortotal:valorGasto};
       consumos[totalcad] = consumo;
 
-   document.getElementById("saida").innerHTML = "Preço combustivel: " + consumos[totalcad].preco +"<br>" + 
+   document.getElementById("saida").innerHTML = "Preço Gasolina: " + consumos[totalcad].precogasolina +"<br>" + 
+                                                "Preço Etanol: " + consumos[totalcad].precoetanol +"<br>" + 
+                                                "Combustível: " + consumos[totalcad].combust +"<br>" + 
                                                 "km por litro: " + consumos[totalcad].kmlitro + "<br>" +
                                                 "Distancia total: " + consumos[totalcad].distanciaEmKm +"<br>" +
                                                  "Valor total gasto: " + valorGasto.toFixed(2);
